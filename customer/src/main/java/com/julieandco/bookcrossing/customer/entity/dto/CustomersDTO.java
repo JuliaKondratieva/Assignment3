591c5abdb9b46bf228b94db61e2eb8d4d2d48d44
Julia <juli4ka.kondratyeva@gmail.com>
2020-10-16 22:47:27 +0300

    Customer

--- customer/src/main/java/com/julieandco/bookcrossing/customer/entity/dto/CustomersDTO.java
+++ customer/src/main/java/com/julieandco/bookcrossing/customer/entity/dto/CustomersDTO.java
@@ -2,10 +2,15 @@ package com.julieandco.bookcrossing.customer.entity.dto;
 
 import com.julieandco.bookcrossing.customer.entity.Customer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomersDTO {
     private List<CustomerDTO> users;
+
+    public CustomersDTO(){
+        users=new ArrayList<>();
+    }
 
     public List<CustomerDTO> getUsers() {
         return users;
